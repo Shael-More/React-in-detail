332489921d8fcb493de0560357a9c25a0f455208
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-06-24 10:52:09 +0530

    hooks in react

--- reacthooks/src/main.jsx
+++ reacthooks/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import UseStateApp from './components/UseStateApp'
-import UseEffectApp from './components/UseEffectApp'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UseStateApp />
-    <UseEffectApp/>
+    <App />
   </React.StrictMode>,
 )
